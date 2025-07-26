0464c757ca3aadc566ebfcedd0e2e136e4f633f3
prp251070 <pratik160804@gmail.com>
2025-07-26 23:17:55 +0530

    main comment and contact us

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+//import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AboutUs from './pages/about_us.jsx';
